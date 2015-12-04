6ff827944972898266de776895b8199461d28724
Hanz <Hansikam94@gmail.com>
2015-12-04 15:18:34 +0530

    initializing database opperator

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/DB/DbOperator.java
@@ -0,0 +1,19 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.DB;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Hanz on 04/12/2015.
+ */
+public class DbOperator extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
